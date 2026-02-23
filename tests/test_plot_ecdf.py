5723de41f87756266a8f67e7758040b9b7a0a087
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2026-02-23 22:51:32 +0000

    Add test for plotting ECDF with random normal data

---
+++ tests/test_plot_ecdf.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from cobtools.plot_utils import ecdf
+
+
+def test_plot_ecdf():
+    data = np.random.normal(loc=0, scale=1, size=1000)
+    fig, ax = plt.subplots()
+    x, y = ecdf.get_ecdf(data)
+    print(x, y)
+    ecdf.plot_ecdf(data, ax=ax)
+    ax.set_ylim(0, 1)
+    plt.show()
+    plt.close(fig)
+
+
+if __name__ == "__main__":
+    test_plot_ecdf()
