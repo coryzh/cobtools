b3a35161b30ed92ba8a336d56448b7f270546535
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2026-02-25 12:42:25 +0000

    Renamed: l -> l_gal, b -> b_gal

--- tests/test_kinematics.py
+++ tests/test_kinematics.py
@@ -16,31 +16,31 @@ class TestEquatorialToGalactic:
     def test_valid_single_number_input(self):
         ra, dec = 103.1, 89.3
         coord = SkyCoord(ra=ra, dec=dec, unit='deg', frame='icrs')
-        l, b = equatorial_to_galactic(ra, dec)
+        gal_l, gal_b = equatorial_to_galactic(ra, dec)
         l_astropy, b_astropy = coord.galactic.l.deg, coord.galactic.b.deg
-        assert isinstance(l, float) and isinstance(b, float)
-        assert np.isclose(l, l_astropy)
-        assert np.isclose(b, b_astropy)
+        assert isinstance(gal_l, float) and isinstance(gal_b, float)
+        assert np.isclose(gal_l, l_astropy)
+        assert np.isclose(gal_b, b_astropy)
 
     def test_valid_list_input(self):
         ra = [10.3, 150.3, 355.1]
         dec = [5.3, 25.1, 88.2]
-        l, b = equatorial_to_galactic(ra, dec)
+        gal_l, gal_b = equatorial_to_galactic(ra, dec)
         coord = SkyCoord(ra=ra, dec=dec, unit='deg', frame='icrs')
         l_astropy, b_astropy = coord.galactic.l.deg, coord.galactic.b.deg
-        print(l, b)
-        assert isinstance(l, np.ndarray) and isinstance(b, np.ndarray)
-        assert l.shape == (len(ra),) and b.shape == (len(dec),)
-        assert np.allclose(l, l_astropy)
-        assert np.allclose(b, b_astropy)
+        print(gal_l, gal_b)
+        assert isinstance(gal_l, np.ndarray) and isinstance(gal_b, np.ndarray)
+        assert gal_l.shape == (len(ra),) and gal_b.shape == (len(dec),)
+        assert np.allclose(gal_l, l_astropy)
+        assert np.allclose(gal_b, b_astropy)
 
     def test_compare_to_astropy(self):
         n_sample = 10000
         np.random.seed(42)
         ra = np.random.uniform(0, 360, size=n_sample)
         dec = np.random.uniform(-90, 90, size=n_sample)
-        l, b = equatorial_to_galactic(ra, dec)
+        gal_l, gal_b = equatorial_to_galactic(ra, dec)
         coord = SkyCoord(ra=ra, dec=dec, unit='deg', frame='icrs')
         l_astropy, b_astropy = coord.galactic.l.deg, coord.galactic.b.deg
-        assert np.allclose(l, l_astropy, atol=5e-3)
-        assert np.allclose(b, b_astropy, atol=5e-3)
+        assert np.allclose(gal_l, l_astropy, atol=5e-3)
+        assert np.allclose(gal_b, b_astropy, atol=5e-3)
