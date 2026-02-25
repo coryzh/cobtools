6167c9363624887f95f0d30af3bfb143027cd36d
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2026-02-25 12:40:14 +0000

    Enhance tests for equatorial_to_galactic function with additional assertions and a new comparison test against astropy

--- tests/test_kinematics.py
+++ tests/test_kinematics.py
@@ -26,6 +26,21 @@ class TestEquatorialToGalactic:
         ra = [10.3, 150.3, 355.1]
         dec = [5.3, 25.1, 88.2]
         l, b = equatorial_to_galactic(ra, dec)
+        coord = SkyCoord(ra=ra, dec=dec, unit='deg', frame='icrs')
+        l_astropy, b_astropy = coord.galactic.l.deg, coord.galactic.b.deg
         print(l, b)
         assert isinstance(l, np.ndarray) and isinstance(b, np.ndarray)
         assert l.shape == (len(ra),) and b.shape == (len(dec),)
+        assert np.allclose(l, l_astropy)
+        assert np.allclose(b, b_astropy)
+
+    def test_compare_to_astropy(self):
+        n_sample = 10000
+        np.random.seed(42)
+        ra = np.random.uniform(0, 360, size=n_sample)
+        dec = np.random.uniform(-90, 90, size=n_sample)
+        l, b = equatorial_to_galactic(ra, dec)
+        coord = SkyCoord(ra=ra, dec=dec, unit='deg', frame='icrs')
+        l_astropy, b_astropy = coord.galactic.l.deg, coord.galactic.b.deg
+        assert np.allclose(l, l_astropy, atol=5e-3)
+        assert np.allclose(b, b_astropy, atol=5e-3)
