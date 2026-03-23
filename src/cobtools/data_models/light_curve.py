PR#26 The propagated error for the mean flux is computed incorrectly. For averaging independent measurements, sigma_mean should scale like sqrt(sum(sigma_i^2)) / N, but the current formula is sqrt(sum(sigma_i^2) / N) (too large by ~sqrt(N)). Please fix the propagation formula used for binned_flux_err.

src/cobtools/data_models/light_curve.py
@@ -119,8 +119,7 @@ class LightCurve:
                 binned_flux_err.append(
                     np.sqrt(
                         np.sum(self.flux_err[mask] ** 2)
-                        / len(self.flux[mask])
-                    )
+                    ) / len(self.flux[mask])
                 )
 
         self.time_axis = np.array(binned_time)