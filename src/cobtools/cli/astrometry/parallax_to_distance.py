50a7817ddd24558153c2b229147316de03887c22
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2026-08-31 16:04:43 +0100

    Added the body for inversion-estimated distances.

--- src/cobtools/cli/astrometry/parallax_to_distance.py
+++ src/cobtools/cli/astrometry/parallax_to_distance.py
@@ -2,6 +2,7 @@
 
 import click
 
+
 @click.command(
     context_settings={"help_option_names": ["-h", "--help"]},
     help="Get parallax-based distance estimates (in kpc)\n\n"
@@ -30,9 +31,35 @@ import click
         "exponential prior for X-ray binaries (e.g., Zhao et al. 2023)."
     )
 )
-def estimate_distance() -> None:
+def estimate_distance(
+    parallax: float, parallax_error: float, method: str
+) -> None:
     """placeholder function for the estimate_distance command."""
-    pass
+    from cobtools.astrometry.distance import (
+        SimpleInversion,
+        XRBExponentialPriorModel
+    )
+    if method == "inv":
+        if parallax < 0 or parallax / parallax_error < 5:
+            click.secho(
+                "Warning: The parallax is negative or has a low SNR (<5). "
+                "The inverted parallax estimate may be unreliable.",
+                fg="yellow",
+                err=True,
+            )
+        dist_mod = SimpleInversion(parallax, parallax_error)
+        click.echo(
+            f"Distance estimate: {dist_mod.d_est: .2f} "
+            f"+/- {dist_mod.d_est_error:.2f} kpc"
+        )
+    elif method == "xrb_exp_prior":
+        dist_mod = XRBExponentialPriorModel(parallax, parallax_error)
+
+    else:
+        raise click.UsageError(
+            f"Unknown method '{method}'. "
+            "Valid options are 'inv' and 'xrb_exp_prior'."
+        )
 
 
 if __name__ == "__main__":
