6b656d0cdac81875c42fe9f5939f603915e90303
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2026-09-02 16:54:37 +0100

    Moved help texts of arguments to docstring.

--- src/cobtools/cli/astrometry/vpec_from_astrometry.py
+++ src/cobtools/cli/astrometry/vpec_from_astrometry.py
@@ -1,80 +1,31 @@
 import click
 import numpy as np
-from cobtools.astrometry.kinematics import peculiar_velocity
+# from cobtools.astrometry.kinematics import peculiar_velocity
 
 
 @click.command(
     context_settings={"help_option_names": ["-h", "--help"]},
-    help=(
-        "Calculate peculiar velocity (vpec) based on from astrometry\n\n"
-        "The user provide the astrometric parameters (ra, dec, pmra, pmdec, "
-        "dist, rv) and the uncertainties on pmra, pmdec, dist, and rv. The "
-        "tool will then generate Monte Carlo samples of the parameters and "
-        "compute the vpec values."
-
-        "The user can also specify the confidence level and the number of "
-        "samples to use in the Monte Carlo simulation.\n\n"
-
-        "The output is the point estimate and uncertainty of the vpec in "
-        "km/s at the given confidence level."
-    )
 )
-@click.argument(
-    "ra", type=click.FloatRange(min=0, max=360), metavar="ra",
-    help=(
-       "Right ascension in decimal degrees. "
-       "Must be in the range [0, 360] degrees."
-    )
-)
-@click.argument(
-    "dec", type=click.FloatRange(min=-90, max=90), metavar="dec",
-    help=(
-        "Declination in decimal degrees. "
-        "Must be in the range [-90, 90] degrees."
-    )
-)
-@click.argument(
-    "pmra", type=float, metavar="pmra",
-    help=(
-        "Proper motion in right ascension (including the cos(dec) factor) "
-        "in mas/yr."
-    )
-)
+@click.argument("ra", type=click.FloatRange(min=0, max=360), metavar="ra")
+@click.argument("dec", type=click.FloatRange(min=-90, max=90), metavar="dec")
+@click.argument("pmra", type=float, metavar="pmra")
 @click.argument(
     "pmra_error", type=click.FloatRange(min=0, min_open=True),
-    help=(
-        "Error in proper motion in right ascension in mas/yr."
-    )
+    metavar="pmra_error",
 )
-@click.argument(
-    "pmdec", type=float, metavar="pmdec",
-    help=(
-        "Proper motion in declination in mas/yr."
-    )
-)
+@click.argument("pmdec", type=float, metavar="pmdec")
 @click.argument(
     "pmdec_error", type=click.FloatRange(min=0, min_open=True),
-    help=(
-        "Error in proper motion in declination in mas/yr."
-    )
+    metavar="pmdec_error",
 )
 @click.argument(
     "dist", type=click.FloatRange(min=0, min_open=True), metavar="dist",
-    help=(
-        "Distance in kpc. Must be a positive value."
-    )
 )
 @click.argument(
     "dist_error", type=click.FloatRange(min=0, min_open=True),
     metavar="dist_error",
 )
-@click.argument(
-    "rv", type=click.FloatRange(min=-3e5, max=3e5), metavar="rv",
-    help=(
-        "Radial velocity in km/s. Must be in the range [-3e5, 3e5] km/s"
-        "(inclusive)."
-    )
-)
+@click.argument("rv", type=click.FloatRange(min=-3e5, max=3e5), metavar="rv")
 @click.argument(
     "rv_error", type=click.FloatRange(min=0, min_open=True),
     metavar="rv_error",
@@ -97,6 +48,35 @@ def calc_vpec(
         dist: float, dist_error: float, rv: float, rv_error: float,
         conf: float, n_samples: int
 ) -> None:
+    """Calculate peculiar velocity (vpec) based on from astrometry
+
+    The user provide the astrometric parameters (ra, dec, pmra, pmdec,
+    dist, rv) and the uncertainties on pmra, pmdec, dist, and rv. The
+    tool will then generate Monte Carlo samples of the parameters and
+    compute the vpec values.
+
+    The user can also specify the confidence level and the number of
+    samples to use in the Monte Carlo simulation.
+
+    \b
+    ra: Right ascension in decimal degrees. Must be in the range
+        [0, 360] degrees.
+    dec: Declination in decimal degrees. Must be in the range
+        [-90, 90] degrees.
+    pmra: Proper motion in right ascension (including the cos(dec)
+        factor) in mas/yr.
+    pmra_error: Error in proper motion in right ascension in mas/yr.
+    pmdec: Proper motion in declination in mas/yr.
+    pmdec_error: Error in proper motion in declination in mas/yr.
+    dist: Distance in kpc. Must be a positive value.
+    dist_error: (no help text was provided for this argument)
+    rv: Radial velocity in km/s. Must be in the range [-3e5, 3e5] km/s
+        (inclusive).
+    rv_error: (no help text was provided for this argument)
+
+    The output is the point estimate and uncertainty of the vpec in
+    km/s at the given confidence level.
+    """
     pass
 
 
